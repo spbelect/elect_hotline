dc97d6282ad5f70ab25ad0dcd234aac097f0e7ed
Evstifeev Roman <someuniquename@gmail.com>
2025-01-20 20:40:10 +0500

    fix vercel_build.sh

--- vercel_build.sh
+++ vercel_build.sh
@@ -2,5 +2,5 @@
 pip3.12 install -r requirements.txt
 #pip3.12 install .
 #python3.12 -c "from importlib import metadata; print(metadata.version('elect-hotline'))"
-python3.12 manage.py collectstatic --noinput
-python3.12 manage.py migrate
+python3.12 ./src/manage.py collectstatic --noinput
+python3.12 ./src/manage.py migrate
