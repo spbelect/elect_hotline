f34b59a11e8cbfe85e7bcd51df84b7d826a8d78a
Evstifeev Roman <someuniquename@gmail.com>
2025-01-27 14:56:03 +0500

    vercel_build.sh - install with pdm instad of using requirements.txt

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,5 +1,7 @@
 #python -m pip install -U pip
-pip3.12 install -r requirements.txt
+pip3.12 install pdm
+python3.12 -m pdm install -v
+#pip3.12 install -r requirements.txt
 #pip3.12 install .
 #python3.12 -c "from importlib import metadata; print(metadata.version('elect-hotline'))"
 python3.12 ./src/manage.py collectstatic --noinput
