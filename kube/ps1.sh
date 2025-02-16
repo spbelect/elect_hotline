6a04ba34c6cc0172281363ae729e290e9f869ee2
Evstifeev Roman <someuniquename@gmail.com>
2025-02-16 11:35:55 +0500

    kube/ps1.sh - download if does not exist

--- kube/ps1.sh
+++ kube/ps1.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 echo -e "Source this file with:\n . ps1.sh"
+
+if [ -f ~/bin/kube-ps1.sh ]; then
+   echo "Sourcing ~/bin/kube-ps1.sh"
+else
+   echo "File ~/bin/kube-ps1.sh does not exist. Downloading now."
+    wget -O ~/bin/kube-ps1.sh https://raw.githubusercontent.com/jonmosco/kube-ps1/refs/heads/master/kube-ps1.sh
+fi
+
+source ~/bin/kube-ps1.sh
 export PS1='\u@\h \w $(kube_ps1)\$'
