d393f8fb9de786da480f98111efb0a32cf3f66d2
Evstifeev Roman <someuniquename@gmail.com>
2025-03-17 21:21:36 +0500

    add test/auth/email_login__test.py

---
+++ test/auth/email_login__test.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import re
+
+from datetime import datetime, date, timedelta
+from time import sleep
+from urllib.parse import parse_qs, urlparse
+
+import django.contrib.auth
+import django.test
+import pendulum
+import pytest
+import respx
+import time_machine
+
+from django.conf import settings
+from django.core import mail
+from django.db import transaction
+from django.urls import reverse
+from django.test import TestCase
+from django.test import override_settings
+from django.utils.timezone import localtime, now
+from loguru import logger
+from model_bakery.baker import make
+from playwright.sync_api import Playwright, Page, expect
+from unittest.mock import Mock, patch, ANY
+
+from ufo.models.base import int16
+from ufo.models import (
+    Region, Country, WebsiteUser, Election, Campaign, Organization, Contact, OrgBranch,
+    WebsiteUser, QuizTopic, Question, Answer, OrgMembership
+)
+
+from .. import base
+from ..base import MSK, ru, spb, msk
+
+
+@pytest.mark.django_db
+@override_settings(EMAIL_BACKEND='anymail.backends.test.EmailBackend')
+def email_login_success__test(client: django.test.Client, ru):
+    """ User should succesfully login with email """
+
+    # WHEN user submits email login form
+    response = client.post('/auth/login', dict(email='user@example.com'))
+
+    # THEN response status is 200 (Success)
+    assert response.status_code == 200
+
+    # AND 1 email is sent
+    assert len(mail.outbox) == 1
+    # to user
+    assert mail.outbox[0].to == ['user@example.com']
+    # AND has a link to sign-in
+    assert mail.outbox[0].body.startswith('Link to sign in: ')
+
+    link = mail.outbox[0].body.split('Link to sign in: ')[-1]
+
+    # WHEN user opens the received link
+    response = client.get(link)
+
+    # THEN response satatus code should be 301 redirect
+    assert response.status_code == 302
+
+    # AND it should redirect to /
+    assert response.url == '/'
+
+    user = django.contrib.auth.get_user(client)
+
+    # AND user is authenticated
+    assert user.is_authenticated
+
+    # AND user email is user@example.com
+    assert user.email == 'user@example.com'
