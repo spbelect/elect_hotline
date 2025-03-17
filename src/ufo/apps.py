d9615afcb291797868b803a551afdafcfd4eb667
Evstifeev Roman <someuniquename@gmail.com>
2025-03-17 22:24:29 +0500

    move checks from apps.py to checks.py

--- src/ufo/apps.py
+++ src/ufo/apps.py
@@ -1,40 +1,6 @@
-# -*- coding: utf-8 -*-
 from django.apps import apps, AppConfig
-from django.conf import settings
-from django.core import checks
-from django.core.mail import EmailMessage
 
-
-@checks.register("email", deploy=True)
-def email_check(app_configs, **kwargs):
-    messages = []
-
-    print(f'Using {settings.EMAIL_BACKEND=}')
-
-    if settings.EMAIL_BACKEND == 'anymail.backends.sendgrid.EmailBackend':
-        if not settings.__dict__.get('SENDGRID_API_KEY'):
-            messages.append(checks.Warning(
-                f'settings.EMAIL_BACKEND is anymail, but settings.SENDGRID_API_KEY is not set',
-                id="Ufo.email.W002"
-            ))
-
-            return messages
-
-    email = EmailMessage(
-        subject = 'Test email from ufo',
-        body = 'Hello world',
-        from_email = f'"Election Hotline" <{settings.DEFAULT_FROM_EMAIL}>',
-        to = [settings.ADMIN_EMAIL]
-    )
-    print(f'Sending email to {settings.ADMIN_EMAIL}')
-    try:
-        email.send()
-    except Exception as err:
-        messages.append(checks.Warning(
-            f'Sending test email failed with {err!r}',
-            id="Ufo.email.W001"
-        ))
-    return messages
+from . import checks
 
 
 class UfoConfig(AppConfig):
